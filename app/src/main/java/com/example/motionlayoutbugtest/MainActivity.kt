ba3716be35b3105fe05d00aa65f2a4bffd90a7e9
Vlad <onlytype62@yandex.ru>
2020-03-11 23:18:59 +0300

    second commit

--- app/src/main/java/com/example/motionlayoutbugtest/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutbugtest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.constraintlayout.motion.widget.MotionLayout
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.btn2)
+        btn.setOnClickListener {
+            when (motionLayout.currentState) {
+                motionLayout.startState -> Toast.makeText(this, "was startState", Toast.LENGTH_SHORT).show()
+                motionLayout.endState -> Toast.makeText(this, "was endState", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this, "was unknownState", Toast.LENGTH_SHORT).show()
+            }
+            if (motionLayout.currentState == motionLayout.endState) {
+                motionLayout.transitionToStart()
+            } else {
+                motionLayout.transitionToEnd()
+            }
+            when (motionLayout.currentState) {
+                motionLayout.startState -> Toast.makeText(this, "now is startState", Toast.LENGTH_SHORT).show()
+                motionLayout.endState -> Toast.makeText(this, "now is endState", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this, "now is unknownState", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onResume() {
